8d32a388fbe4e198902be100b5508cc490974f5b
Josh <joshuapenuela12@gmail.com>
2025-10-27 22:43:14 +0800

    1st Update of Software Section in Home

--- components/software.tsx
+++ components/software.tsx
@@ -2,12 +2,76 @@ import React from 'react'
 
 export default function Software() {
   return (
-    <section className='w-full my-10'>
-        <div>
-            <div className='w-full m-auto place-items-center-safe'>
-                <h1 className='text-4xl font-bold '>Software</h1>
+    <section className='my-10 w-full'>
+      <div>
+        <div className='m-auto w-full'>
+          <div className='place-items-center-safe pb-10'>
+            <h1 className='text-4xl font-bold'>Software</h1>
+          </div>
+          <div className='grid grid-cols-5 gap-5 border-2 mx-40'>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Next.js</h4>
             </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>React</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>TailwindCSS</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Node.js</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Laravel</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>npm</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>HTML5</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>CSS</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>JavaScript</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>PHP</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>MySQL</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>TypeScript</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Postman</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Vercel</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Git</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Android Studio</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Github</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Figma</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Adobe Photoshop</h4>
+            </div>
+            <div className='flex flex-row py-4 rounded-md border'>
+              <h4 className='text-xl'>Canva</h4>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
