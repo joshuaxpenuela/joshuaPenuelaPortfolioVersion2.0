410e6d2853632198b94522aab5418962af219e5a
Josh <joshuapenuela12@gmail.com>
2025-11-01 01:54:44 +0800

    graphics and projects page minor update

--- components/graphics.tsx
+++ components/graphics.tsx
@@ -5,7 +5,7 @@ export default function Graphics() {
     <section className='flex flex-col items-center pb-24 md:flex-row md:items-center dark:bg-linear-to-t dark:from-zinc-950 dark:via-zinc-900 dark:to-zinc-800/80'>
       <div className='grid w-full place-content-center-safe border-2 text-black dark:text-white'>
         <div className='w-full place-items-center-safe py-40 font-black'>
-          <h1 className='text-5xl'>Graphics</h1>
+          <h1 className='text-5xl'>Graphic Designs</h1>
         </div>
         <div className='w-full content-center'>
           <p className='text-center'>Under Development...</p>
--- components/projects.tsx
+++ components/projects.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
     <section className='flex flex-col items-center pb-24 md:flex-row md:items-center dark:bg-linear-to-t dark:from-zinc-950 dark:via-zinc-900 dark:to-zinc-800/80'>
       <div className='grid w-full place-content-center-safe text-black dark:text-white border-2'>
         <div className='font-black place-items-center-safe w-full py-40'>
-          <h1 className='text-5xl'>Projects</h1>
+          <h1 className='text-5xl'>Software Projects</h1>
         </div>
         <div className='w-full content-center'>
           <p className='text-center'>Under Development...</p>
