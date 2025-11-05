0f62faf7797de9e103797d1183d67774a95ef182
Nays Wan <joshuapenuela12@gmail.com>
2025-11-05 16:35:30 +0800

    Updated the graphic section layout

--- components/graphics.tsx
+++ components/graphics.tsx
@@ -1,14 +1,85 @@
 import React from 'react'
+import Software from './software'
+
+const GraphicDesigns = [
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  },
+  {
+    name: 'img',
+    image: '',
+    software: ''
+  }
+]
 
 export default function Graphics() {
   return (
-    <section className='flex flex-col items-center pb-24 md:flex-row md:items-center dark:bg-linear-to-t dark:from-zinc-950 dark:via-zinc-900 dark:to-zinc-800/80'>
-      <div className='grid w-full place-content-center-safe text-black dark:text-white'>
-        <div className='w-full place-items-center-safe py-40 font-black'>
-          <h1 className='text-5xl'>Graphic Designs</h1>
+    <section className='flex w-full flex-col items-center pb-24 md:flex-row md:items-center dark:bg-linear-to-t dark:from-zinc-950 dark:via-zinc-900 dark:to-zinc-800/80'>
+      <div className='w-full text-black dark:text-white'>
+        <div className='mt-40 mb-20 w-full font-black'>
+          <h1 className='text-center text-5xl'>Graphic Designs</h1>
         </div>
-        <div className='w-full content-center'>
-          <p className='text-center'>Under Development...</p>
+        <div className='w-full pt-20'>
+          <div className='mx-40 grid grid-cols-4 gap-x-10 gap-y-10'>
+            {GraphicDesigns.map(graphicimage => (
+              <div className='place-items-center-safe rounded-lg border-2'>
+                {graphicimage.image}
+                {graphicimage.name}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
