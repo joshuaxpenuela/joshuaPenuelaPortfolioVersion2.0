63bb8527f3f380934807b90dd8f0a5c71c12f5c5
Josh <joshuapenuela12@gmail.com>
2025-11-08 10:26:07 +0800

    Minor fix in the graphics page bug

--- app/graphics/page.tsx
+++ app/graphics/page.tsx
@@ -3,18 +3,18 @@ import Graphics from '@/components/graphics'
 import { ShootingStars } from '@/components/ui/shooting-stars'
 import { StarsBackground } from '@/components/ui/stars-background'
 
+
 export default function GraphicsPage() {
   return (
-    <section className="relative w-full h-full overflow-hidden">
-      {/* --- Background Layers --- */}
-      <div className="fixed inset-0 -z-10">
-        <StarsBackground className="bg-scroll" />
-        <ShootingStars className="bg-scroll" />
+    <section className='mx-auto w-full'>
+      <div className='fixed inset-0 -z-10'>
+        <StarsBackground className='bg-scroll' />
+        <ShootingStars className='bg-scroll' />
       </div>
-
-      {/* --- Foreground Content --- */}
-      <div className="relative z-10 mx-auto flex flex-col">
-        <Graphics />
+      <div className='mx-auto flex flex-col'>
+        <div>
+          <Graphics />
+        </div>
       </div>
     </section>
   )
